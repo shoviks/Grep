17f61c38f032d47ef69c060eac506b2f9d088392
shosho161@gmail.com <shosho161@gmail.com>
2015-01-12 13:02:00 -0500

    Simulates grep command
    Includes -v and -x options

---
+++ grep.py
@@ -0,0 +1,33 @@
+import sys
+import re
+
+if len(sys.argv) == 3:
+    pattern = sys.argv[1]
+
+    file = open( sys.argv[2], "r" ).readlines()
+
+    for line in file:
+        """Prints line if pattern is found is line."""
+        if re.search(pattern, line):
+            print (line)
+
+elif len(sys.argv) == 4:
+    if sys.argv[1] == "-v":
+        pattern = sys.argv[2]
+        file = open( sys.argv[3], "r" ).readlines()
+
+        for line in file:
+            """Prints line if pattern is not found in line."""
+            if not re.search(pattern, line):
+                print (line)
+    
+    elif sys.argv[1] == "-x":
+        pattern = sys.argv[2]
+        file = open( sys.argv[3], "r" ).readlines()
+
+        for line in file:
+            """Prints line if pattern matches line exactly."""
+            if pattern == line:
+                print (line)
+
+ 
